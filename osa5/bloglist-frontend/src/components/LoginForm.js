2db435dd5dba55679d13976225645722978f4485
Sami Eriksson <sami.eriksson@yahoo.fi>
2021-07-04 00:55:50 +0300

    Excercises 5.17-5.18

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -18,6 +18,7 @@ const LoginForm = ({
                     <input
                         value={username}
                         onChange={handleUsernameChange}
+                        id="username"
                     />
                 </div>
                 <div>
@@ -26,9 +27,10 @@ const LoginForm = ({
                         type="password"
                         value={password}
                         onChange={handlePasswordChange}
+                        id="password"
                     />
                 </div>
-                <button type="submit">login</button>
+                <button type="submit" id="login-button">login</button>
             </form>
         </div>
     )
